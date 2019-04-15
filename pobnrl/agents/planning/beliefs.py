5c2fd20579264917a4d206391d1cbc0006bb704c
sammie katt <sammie.katt@gmail.com>
2019-04-15 16:25:01 -0400

    implemented and tested belief updates

--- pobnrl/agents/planning/beliefs.py
+++ pobnrl/agents/planning/beliefs.py
@@ -6,8 +6,9 @@ Contains
 
 """
 
-from typing import Any
+from typing import Any, Callable
 import abc
+import copy
 import random
 
 
@@ -31,6 +32,14 @@ class ParticleFilter(abc.ABC):
 
         RETURNS (`Any`): particle
 
+        """
+
+    @property
+    @abc.abstractmethod
+    def size(self) -> int:
+        """ returns size of particle fitler (number of samples)
+
+        RETURNS (`int`):
         """
 
 
@@ -60,6 +69,14 @@ class FlatFilter(ParticleFilter):
 
         """
         return random.choice(self._particles)
+
+    @property
+    def size(self) -> int:
+        """ returns size of particle fitler (number of samples)
+
+        RETURNS (`int`):
+        """
+        return len(self._particles)
 
 
 class WeightedParticle():
@@ -91,6 +108,18 @@ class WeightedParticle():
         """
         return self._weight
 
+    @weight.setter
+    def weight(self, weight: float):
+        """ sets weight of particles
+
+        Args:
+             weight: (`float`): must be > 0
+
+        """
+
+        assert weight > 0
+        self._weight = weight
+
     @property
     def value(self) -> Any:
         """ returns the value of the particle
@@ -101,6 +130,17 @@ class WeightedParticle():
 
         """
         return self._val
+
+    @value.setter
+    def value(self, value: Any):
+        """ sets value of particle
+
+        Args:
+             value: (`Any`):
+
+        """
+
+        self._val = value
 
 
 class WeightedFilter(ParticleFilter):
@@ -149,3 +189,72 @@ class WeightedFilter(ParticleFilter):
             f"sampled weight {rand} (< stored total weight",
             f"{self._total_weight}) > real total weight {acc}"
         )
+
+    @property
+    def size(self) -> int:
+        """ returns size of particle fitler (number of samples)
+
+        RETURNS (`int`):
+        """
+        return len(self._particles)
+
+    def __iter__(self):
+        """ iterate over samples in filter """
+        return iter(self._particles)
+
+
+def rejection_sampling(
+        particle_filter: ParticleFilter,
+        update_f: Callable[[Any], Any],
+        accept_f: Callable[[Any], bool]) -> ParticleFilter:
+    """ applies rejection sampling on particle_filter
+
+    will continuously update and filter samples until a new particle filter of
+    the same size is created
+
+    Args:
+         particle_filter: (`pobnrl.agents.planning.beliefs.ParticleFilter`): input belief
+         update_f: (`Callable[[Any], Any]`): function to update a sample
+         accept_f: (`Callable[[Any], bool]`): function to accept/reject sample
+
+    RETURNS (`pobnrl.agents.planning.beliefs.ParticleFilter`): updated belief
+
+    """
+
+    new_pf = type(particle_filter)()
+
+    while new_pf.size < particle_filter.size:
+
+        sample = copy.deepcopy(particle_filter.sample())
+        updated_sample = update_f(sample)
+
+        if accept_f(updated_sample):
+            new_pf.add_particle(updated_sample)
+
+    return new_pf
+
+
+def importance_sampling(
+        particle_filter: WeightedFilter,
+        update_f: Callable[[Any], Any],
+        weight_f: Callable[[Any], float]) -> WeightedFilter:
+    """ returns a updated weighted filter according to provided functions
+
+    updates **all** samples in particle_filter and saves them with an
+    associated weight into a particle fitler
+
+    Args:
+         particle_filter: (`pobnrl.agents.planning.beliefs.WeightedFilter`): initial particle filter
+         update_f: (`Callable[[Any], Any]`):
+         weight_f: (`Callable[[Any], float]`):
+
+    RETURNS (`pobnrl.agents.planning.beliefs.WeightedFilter`):
+
+    """
+
+    for sample in particle_filter:
+
+        sample.value = update_f(sample.value)
+        sample.weight *= weight_f(sample.value)
+
+    return particle_filter
--- pobnrl/tests/test_beliefs.py
+++ pobnrl/tests/test_beliefs.py
@@ -2,8 +2,8 @@
 
 import unittest
 
-from agents.planning import FlatFilter, WeightedFilter
-from agents.planning.beliefs import WeightedParticle
+from agents.planning.beliefs import FlatFilter, WeightedFilter, WeightedParticle
+from agents.planning.beliefs import rejection_sampling, importance_sampling
 
 
 class TestFilters(unittest.TestCase):
@@ -54,6 +54,76 @@ class TestFilters(unittest.TestCase):
 
         wfilter.add_particle(WeightedParticle(1, 10000000))
         self.assertEqual(wfilter.sample(), 1)
+
+
+class TestBeliefUpdates(unittest.TestCase):
+    """ tests belief updates """
+
+    @staticmethod
+    def increment(some_num: int):
+        """ increments input int `some_num` """
+        return some_num + 1
+
+    @staticmethod
+    def accept_all(_):
+        """ returns true for all input """
+        return True
+
+    @staticmethod
+    def one(_):
+        """ returns 1 for all input """
+        return 1
+
+    @staticmethod
+    def over_one(some_num: int):
+        """ returns 1/`sum_num` """
+        return 1 / float(some_num)
+
+    @staticmethod
+    def is_odd(some_num: int):
+        """ returns true if input int x is odd """
+        return not some_num % 2 == 0
+
+    def test_rejection_sampling(self):
+        """ tests the rejection sampling way of updating belief """
+
+        ffilter = FlatFilter()
+        for _ in range(5):
+            ffilter.add_particle(0)
+
+        ffilter1 = rejection_sampling(ffilter, self.increment, self.accept_all)
+
+        self.assertEqual(ffilter1.sample(), 1)
+
+        ffilter1.add_particle(10)
+        ffilter2 = rejection_sampling(ffilter1, self.increment, self.is_odd)
+
+        self.assertEqual(ffilter2.sample(), 11)
+        self.assertIn(ffilter1.sample(), [1, 10])
+
+    def test_importance_sampling(self):
+        """ tests the importance sampling way of updating belief """
+
+        wfilter = WeightedFilter()
+        for _ in range(5):
+            wfilter.add_particle(WeightedParticle(value=1, weight=.5))
+
+        wfilter1 = importance_sampling(wfilter, self.increment, self.one)
+
+        for particle in wfilter1:
+            self.assertEqual(particle.weight, .5)
+            self.assertEqual(particle.value, 2)
+
+        wfilter1.add_particle(WeightedParticle(value=10, weight=5))
+        wfilter2 = importance_sampling(wfilter1, self.increment, self.over_one)
+
+        for particle in wfilter2:
+            self.assertIn(particle.value, [11, 3])
+
+            if particle.value == 11:
+                self.assertEqual(particle.weight, 5 * (1 / 11))
+            else:
+                self.assertAlmostEqual(particle.weight, .5 * (1 / 3))
 
 
 if __name__ == '__main__':
