b1762ac4438ed7ab892397482d43b54045a9e2d4
sammie katt <sammie.katt@gmail.com>
2020-12-18 18:01:31 -0500

    init visualization refactory

--- scripts/analysis/plotting.py
+++ scripts/analysis/plotting.py
@@ -1,15 +1,10 @@
 """ plotting functionality """
 
 import sys
-
 from typing import List
-import matplotlib
-import matplotlib.pyplot as plt
-import more_itertools as mitt
-import numpy as np
 
+from po_nrl.analysis.visualization import default_plot_style, plot_experiment
 
-# matplotlib.rc('font', size=20)
 
 def main(smoothing_argument: str, files: List[str]) -> None:
     """ main: validates user input and plots files
@@ -25,36 +20,19 @@ def main(smoothing_argument: str, files: List[str]) -> None:
     try:
         smoothing = int(smoothing_argument)
     except ValueError:
-        print(f"{smoothing_argument} cannot be interpret as amount of smoothing (int)")
+        print(
+            f"{smoothing_argument} cannot be interpret as amount of smoothing (int)"
+        )
 
     plot_experiment(files, smooth_amount=smoothing)
-
-
-def plot_experiment(file_names: List[str], smooth_amount: int = 25) -> None:
-    """ plot_experiment plots returns of experiment return files
-
-    Args:
-         file_name (`List[str]`): file paths
-
-    """
-
-    #   colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b']
-
-    for i, file_name in enumerate(file_names):
-        returns = np.loadtxt(file_name, delimiter=',')[:, 0].tolist()
-
-        returns = np.mean(list(mitt.windowed(returns, smooth_amount)), axis=1)
-
-        plt.plot(returns, label=file_name)
-        # plt.plot(returns, label=file_name, color=colors[i])
-
-    plt.tight_layout()
-    plt.legend()
+    default_plot_style()
     plt.show()
 
 
 if __name__ == "__main__":
 
-    assert len(sys.argv) > 2, "Expects at least 2 arguments: # smoothing and list fo files"
+    assert (
+        len(sys.argv) > 2
+    ), "Expects at least 2 arguments: # smoothing and list fo files"
 
     main(sys.argv[1], sys.argv[2:])
